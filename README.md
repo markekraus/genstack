d5965061c5484e15b9b7df2b4149e69f367e8fbe
Mark Kraus <github@markekraus.com>
2022-03-19 10:10:33 -0700

    Update Readme and Tests with sturct examples

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ import (
     genstack "github.com/markekraus/genstack/pkg"
 )
 
+type mytype struct {
+    a, b int
+}
+
 func main() {
     s := genstack.New[int]()
     fmt.Printf("elements: %v, want: %v\n", s.Len(), 0)
@@ -22,9 +26,17 @@ func main() {
     s.Push(90)
     fmt.Printf("elements: %v, want: %v\n", s.Len(), 3)
     for i := s.Len(); i > 0; i-- {
-        m := s.Pop()
+        m := s.Pop().Value
         fmt.Printf("elements: %v, want: %v\n", s.Len(), i)
-        fmt.Printf("Value: %v\n", m.Value)
+        fmt.Printf("Value: %v\n", m)
     }
+
+    s2 := genstack.New[*mytype*]()
+    e1 := &mytype{1, 2}
+    s2.Push(&mytype{1, 2})
+    e2 := q.Pop().Value
+    fmt.Printf("%v\n", e2 == e1)
+    fmt.Printf("%v\n", e2.a == e1.a)
+    fmt.Printf("%v\n", e2.b == e1.b)
 }
 ```
--- pkg/genstack_test.go
+++ pkg/genstack_test.go
@@ -10,18 +10,22 @@ func checkStackLen[T any](t *testing.T, s *Stack[T], len int) bool {
 	return true
 }
 
+type myType struct {
+	a, b int
+}
+
 func TestStack(t *testing.T) {
 	s := New[bool]()
-	checkStackLen[bool](t, s, 0)
+	checkStackLen(t, s, 0)
 }
 
 func TestPush(t *testing.T) {
 	s := New[bool]()
-	checkStackLen[bool](t, s, 0)
+	checkStackLen(t, s, 0)
 	s.Push(true)
-	checkStackLen[bool](t, s, 1)
+	checkStackLen(t, s, 1)
 	s.Push(false)
-	checkStackLen[bool](t, s, 2)
+	checkStackLen(t, s, 2)
 }
 
 func TestPop(t *testing.T) {
@@ -30,25 +34,42 @@ func TestPop(t *testing.T) {
 	s.Push(false)
 	s.Push(true)
 	s.Push(false)
-	checkStackLen[bool](t, s, 4)
+	checkStackLen(t, s, 4)
 	e := s.Pop()
-	checkStackLen[bool](t, s, 3)
+	checkStackLen(t, s, 3)
 	if e.Value != false {
 		t.Errorf("e.Value = %v, want %v", e.Value, false)
 	}
 	e = s.Pop()
-	checkStackLen[bool](t, s, 2)
+	checkStackLen(t, s, 2)
 	if e.Value != true {
 		t.Errorf("e.Value = %v, want %v", e.Value, true)
 	}
 	e = s.Pop()
-	checkStackLen[bool](t, s, 1)
+	checkStackLen(t, s, 1)
 	if e.Value != false {
 		t.Errorf("e.Value = %v, want %v", e.Value, false)
 	}
 	e = s.Pop()
-	checkStackLen[bool](t, s, 0)
+	checkStackLen(t, s, 0)
 	if e.Value != true {
 		t.Errorf("e.Value = %v, want %v", e.Value, true)
+	}
+}
+
+func TestStruct(t *testing.T) {
+	e1 := &myType{1, 2}
+	s := New[*myType]()
+	s.Push(e1)
+	checkStackLen(t, s, 1)
+	e2 := s.Pop().Value
+	if e2 != e1 {
+		t.Errorf("e2 = %v, want %v", e2, e1)
+	}
+	if e2.a != 1 {
+		t.Errorf("e2.a = %v, want %v", e2.a, 1)
+	}
+	if e2.b != 2 {
+		t.Errorf("e2.b = %v, want %v", e2.b, 2)
 	}
 }
